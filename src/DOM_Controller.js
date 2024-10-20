7252db1645f7fb17fd2705088d7a9a6ba7ae9528
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-10-20 21:27:20 +0500

    change element genrator function
    Removed the parameter and function to append child

--- src/DOM_Controller.js
+++ src/DOM_Controller.js
@@ -4,7 +4,7 @@ class displayController {
 
     //Element generator
 
-    elementGenerator(type, attributes, textContent, parentElement) {
+    elementGenerator(type, attributes = {}, textString) {
 
         const DOMElement = document.createElement(type);
 
@@ -12,17 +12,29 @@ class displayController {
             DOMElement.setAttribute(key, attributes[key]);
         });
 
-        DOMElement.textContent = textContent;
-
-        parentElement.appendChild(DOMElement)
+        DOMElement.textContent = textString;
     }
 };
 
 displayController.prototype.contentBox = document.querySelector('#content');
 
 
+/* <div class="hero-card">
+<div class="hero-left">
+    <h1 class="hero-title">Big Flavor in Every Bite</h1>
+    <p class="hero-text">Discover a delightful variety of freshly crafted small plates, perfect for sharing or savoring on your own. At Small Bites, we turn simple ingredients into unforgettable tastes.</p>
+</div>
+<div class="hero-right">
+    <img src="./asset/resource/pizza.png" alt="">
+</div>
+</div> */
+
 // home display class
 class homeRender extends displayController {
     
+// render homepage
+    renderHome() {
+        this.elementGenerator('div',{class: "hero-card"}, this.contentBox, "");
 
+    }
 };
