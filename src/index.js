92b18c6ad1aaf9711f9ac2fed622f5acb1e3b98a
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-10-22 02:59:41 +0500

    add menu data

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import "./style.css";
 import { HomepageClass } from "./displayController";
 import { MenuPageClass } from "./displayController";
 import { UIController } from "./UIController";
+import { MenuData } from "./menuData";
 
 const homePageObj = new HomepageClass();
 const menuPageObj = new MenuPageClass();
 const UIControllerObj = new UIController();
+const menuDataObj = new MenuData();
 
 function init() {
     homePageObj.renderHome();
-    UIControllerObj.navBarClick(homePageObj.renderHome, menuPageObj.renderMenuPage);
+    UIControllerObj.navBarClick(homePageObj.renderHome, () => menuPageObj.renderMenuPage(menuDataObj.dishCat(), menuDataObj.dishData()));
 };
 
 init();
