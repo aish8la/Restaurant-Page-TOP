b611468cccadaea633fb91cf049dbea1e62cd5d5
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-10-21 23:45:05 +0500

    add navBarclick function

--- src/UIController.js
+++ src/UIController.js
@@ -2,4 +2,22 @@ export class UIController {
     constructor() {
         const navBar = document.querySelector('header > nav');
     }
+
+    navBarClick(home, menu, about) {
+        this.navBar.addEventListner('click', e => {
+            const btnId = e.target.id;
+            
+            if(e.target.id === 'home-btn') {
+                home();
+            };
+
+            if(e.target.id === 'menu-btn') {
+                menu();
+            };
+
+            if(e.target.id === 'about-btn') {
+                about();
+            };
+        });
+    }
 }
